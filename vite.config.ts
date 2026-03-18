2ce706442406051129a7706ca728ec05d50700ca
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2026-03-18 16:34:59 +0530

    Base change to custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Omega-Tutorials/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
