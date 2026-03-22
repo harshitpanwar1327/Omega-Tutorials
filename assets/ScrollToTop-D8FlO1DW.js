21d805aed04d074aaf9f8b25e28076a667e7b3da
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2026-03-23 03:12:25 +0530

    Updates

---
+++ assets/ScrollToTop-D8FlO1DW.js
@@ -0,0 +1,1 @@
+import{u as t,b as e}from"./router-2yig_bLX.js";const s=()=>{const{pathname:o}=t();return e.useEffect(()=>{window.scrollTo({top:0,left:0,behavior:"smooth"})},[o]),null};export{s as default};
